6d07030e298243e4947690db87bbf9c37b53841e
Daniel Cabral <danicabral15@outlook.com>
2025-02-28 21:17:48 -0300

    feat: update form validation and error handling in ItemsForm component

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,16 @@ import { useDisclosure } from "@mantine/hooks";
 import { ItemsList } from "./components/ItemsList";
 import { ItemsForm } from "./components/ItemsForm";
 import { useLocalStorage } from "./hooks/useLocalStorage";
+import * as z from "zod";
+
+const formSchema = z
+  .string()
+  .min(3, { message: "El item debe tener al menos 3 caracteres" });
 
 export const App = () => {
   const [items, setItems] = useLocalStorage<string[]>("items", []);
   const [itemName, setItemName] = useState("");
+  const [error, setError] = useState<string | null>(null);
   const [opened, { open, close }] = useDisclosure(false);
 
   const handleOnDelete = (id: number) => {
@@ -17,16 +23,22 @@ export const App = () => {
   };
 
   const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setError(null);
     setItemName(e.target.value);
   };
 
   const handleOnSubmit = (e: React.FormEvent) => {
     e.preventDefault();
 
-    if (itemName.trim()) {
+    const resultado = formSchema.safeParse(itemName);
+
+    if (resultado.success && itemName.trim()) {
       setItems([...items, itemName]);
       setItemName("");
+      setError(null);
       close();
+    } else {
+      setError(resultado.error?.issues[0]?.message || "El item no es válido");
     }
   };
 
@@ -52,6 +64,7 @@ export const App = () => {
               itemName={itemName}
               handleOnSubmit={handleOnSubmit}
               handleOnChange={handleOnChange}
+              error={error}
             />
           </Modal>
 
--- src/components/ItemsForm.tsx
+++ src/components/ItemsForm.tsx
@@ -1,26 +1,36 @@
-import { Button, Group, TextInput } from "@mantine/core";
+import { Button, Group, Text, TextInput } from "@mantine/core";
 
 interface ItemsFormProps {
   itemName: string;
   handleOnSubmit: (e: React.FormEvent) => void;
   handleOnChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  error: string | null;
 }
 
 export const ItemsForm = ({
   itemName,
   handleOnSubmit,
   handleOnChange,
+  error,
 }: ItemsFormProps) => {
   return (
     <form onSubmit={handleOnSubmit}>
-      <Group mt="xl" justify="center" grow>
+      {error && (
+        <Text c="red" mb="md" size="sm">
+          {error}
+        </Text>
+      )}
+      <Group mt="md" justify="center" grow>
         <TextInput
           aria-label="Nombre del producto a agregar"
           placeholder="Puré de tomate"
           value={itemName}
           onChange={handleOnChange}
-        ></TextInput>
-        <Button type="submit">Agregar</Button>
+          error={error ? true : false}
+        />
+        <Button type="submit" disabled={!!error}>
+          Agregar
+        </Button>
       </Group>
     </form>
   );
